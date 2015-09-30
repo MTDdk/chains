76f51c94fe0942a71e3ac057b5b0973f410ea300
MTD <an@unsilo.com>
2015-09-30 13:47:31 +0200

    elaborated on the example

--- src/examples/java/chains/ExampleMain.java
+++ src/examples/java/chains/ExampleMain.java
@@ -1,34 +1,56 @@
 package chains;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 import net.javapla.chains.Try;
 
 public class ExampleMain {
 
     public static void main(String[] args) {
-        Try
-        .with(() ->(Connection)null)
-        .with((Connection conn) -> conn.createStatement().executeQuery(""))
-        .perform((ResultSet rs) -> { rs.next();})
-        .perform(() -> System.out.println(""))
-        .perform(() -> 42);
+        Integer result = 
+                Try.perform(() -> 42)
+                   .perform((Integer i)-> i+10)
+                   .execute().get();
+        System.out.println(result); // 52
+        
+        
+        result = Try.with(() -> new TestResource())
+                    .perform((TestResource stream) -> stream.increment(42) )
+                    .execute() // closing the stream(s)
+                    .get(); // Optional.get()
+        System.out.println(result); // Closed with result: 43
+        
+        
+        Try.with(() -> new TestResource())
+           .with((TestResource stream) -> stream.openConnection("testing") ) // using the stream and producing another stream
+           .perform((ByteArrayInputStream stream ) -> stream.available())
+           .perform((Integer number) -> System.out.print("Number of characters in \"testing\": " + number + " - "))
+           .execute() // closing the stream(s)
+           ;
+        // Output: 
+        // Number of characters in "testing": 7 - Closed with result: ByteArrayInputStream closed
+    }
     
-        Try
-        .with(() ->(Connection)null)
-        .with((Connection conn) -> conn.createStatement().executeQuery(""))
-        ;
+    private static class TestResource implements AutoCloseable {
         
+        public int increment(int value) {
+            return ++value;
+        }
         
-        Try
-        .with(() ->(Connection)null)
-        .with((Connection conn) -> conn.createStatement().executeQuery(""))
-        .perform((ResultSet rs) -> rs.next());
+        public ByteArrayInputStream openConnection(String s) {
+            return new ByteArrayInputStream(s.getBytes()) {
+                @Override
+                public void close() throws IOException {
+                    super.close();
+                    System.out.println("ByteArrayInputStream closed");
+                }
+            };
+        }
         
-        Try
-        .perform(() -> 42)
-        .perform((Integer i)-> i+10)
-        .execute();
+        @Override
+        public void close() throws Exception {
+            System.out.print("Closed with result: ");
+        }
     }
 }
